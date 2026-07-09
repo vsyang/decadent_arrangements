ba6c9abd40e3109b720d71d1c57de966234b2506
Joelnin <joelninr@gmail.com>
2026-07-08 20:34:59 -0400

    Set correct names for actions

--- src/components/layout/NavActionButton.tsx
+++ src/components/layout/NavActionButton.tsx
@@ -62,10 +62,10 @@ export function NavActionButton() {
 
     return (
         <Link
-            href={session.isAuthenticated ? "/orders" : "/api/auth/signin"}
+            href={session.isAuthenticated ? "/" : "/api/auth/signin"}
             className="rounded-full bg-stone-100 px-4 py-1.5 text-xs text-stone-900 font-semibold hover:opacity-90 transition-opacity border border-stone-200"
         >
-            {session.isAuthenticated ? "My Orders" : "Orders"}
+            {session.isAuthenticated ? "Sign out" : "Sign up"}
         </Link>
     );
 }
