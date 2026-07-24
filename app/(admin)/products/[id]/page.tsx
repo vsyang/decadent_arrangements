89f72c6dd522bfc837347760d1326330c4a05e31
vsyang <yang.vanessa85@gmail.com>
2026-07-23 18:11:14 -0600

    got rid of "size" on product [id]

--- app/(admin)/products/[id]/page.tsx
+++ app/(admin)/products/[id]/page.tsx
@@ -68,10 +68,6 @@ export default async function ProductDetailsPage({
 
         <div className="mt-4 flex flex-wrap gap-3 text-sm">
           <span className="rounded-full bg-slate-100 px-4 py-2 font-medium text-slate-700">
-            <b>Size:</b> {product.id}
-          </span>
-
-          <span className="rounded-full bg-slate-100 px-4 py-2 font-medium text-slate-700">
             <b>Capacity:</b> {product.capacity}
           </span>
 
