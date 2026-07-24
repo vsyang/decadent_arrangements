8cb093faefbdce70411316f97e45589a75ab3d9a
Joelnin <joelninr@gmail.com>
2026-07-24 12:38:00 -0400

    Fixing form issues

--- app/(admin)/orders/new/OrderForm.tsx
+++ app/(admin)/orders/new/OrderForm.tsx
@@ -125,7 +125,6 @@ export default function OrderForm({
               type="email"
               name="email"
               required
-              disabled={!isAdmin}
               defaultValue={savedCustomer?.email ?? ""}
               className="w-full rounded-md border border-[#807973]/40 px-3 py-2 text-black focus:border-[#03989e] focus:outline-none focus:ring-2 focus:ring-[#03989e]/30"
             />
